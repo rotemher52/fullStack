43065a39e7471b7fd3ae88c92d176e8fd316927c
Ploni Almoni <abc@example.com>
2023-06-11 09:14:34 +0300

    added

--- lesson24-8.6.23/part1/script.ts
+++ lesson24-8.6.23/part1/script.ts
@@ -1,4 +1,4 @@
-import { Student } from './scripts/students';
+// import './scripts/students';
 
 function sum(num1: number, num2: number): number {
     return num1 + num2;
--- lesson24-8.6.23/part1/scripts/script.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sum(num1, num2) {
-    return num1 + num2;
-}
-var num = sum(5, 10);
